534823813d03e3e2226af8a079defe9061d2255e
Jens Reimann <jreimann@redhat.com>
2017-09-14 15:36:42 +0200

    Add a bit of client testing with the local client

---
+++ device-registry-client/src/test/java/iot/core/services/device/registry/client/util/LocalTest.java
@@ -0,0 +1,55 @@
+package iot.core.services.device.registry.client.util;
+
+import static java.util.Collections.emptyMap;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.collect.Maps;
+
+import iot.core.services.device.registry.client.LocalClient;
+import iotcore.service.device.AlwaysPassingDeviceSchemaValidator;
+import iotcore.service.device.Device;
+import iotcore.service.device.InMemoryDeviceRegistry;
+
+public class LocalTest {
+
+    private LocalClient client;
+
+    @Before
+    public void setup() {
+        this.client = new LocalClient(new InMemoryDeviceRegistry(new AlwaysPassingDeviceSchemaValidator()));
+    }
+
+    @After
+    public void cleanup() throws Exception {
+        this.client.close();
+    }
+
+    @Test
+    public void test1() {
+
+        final Device device1 = new Device("id1", new Date(), new Date(), "type1", emptyMap());
+
+        this.client.sync().save(device1);
+
+        final Device device2 = this.client.sync().findById("id1").get();
+
+        assertDevice(device1, device2);
+    }
+
+    private void assertDevice(final Device expected, final Device actual) {
+        assertEquals(expected.getDeviceId(), actual.getDeviceId());
+        assertEquals(expected.getType(), actual.getType());
+        assertEquals(expected.getCreated(), actual.getCreated());
+        assertEquals(expected.getUpdated(), actual.getUpdated());
+
+        assertTrue(Maps.difference(expected.getProperties(), actual.getProperties()).areEqual());
+    }
+
+}
