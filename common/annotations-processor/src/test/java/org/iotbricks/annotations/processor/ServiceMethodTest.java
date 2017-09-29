32c5e6dfa85a2e9c0973f5d4ef3ff3af618ceb32
Jens Reimann <jreimann@redhat.com>
2017-09-29 13:40:30 +0200

    Fully generate the common client parts

---
+++ common/annotations-processor/src/test/java/org/iotbricks/annotations/processor/ServiceMethodTest.java
@@ -0,0 +1,23 @@
+package org.iotbricks.annotations.processor;
+
+import org.iotbricks.annotations.processor.ServiceMethod.TypeName;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ServiceMethodTest {
+
+    @Test
+    public void test1() {
+        assertTypeName("foo.Bar", "foo", "Bar");
+        assertTypeName("foo.baz.Bar", "foo.baz", "Bar");
+        assertTypeName("Bar", "", "Bar");
+    }
+
+    private static void assertTypeName(final String typeName, final String expectedPackageName,
+            final String expectedSimpleName) {
+        final TypeName type = new ServiceMethod.TypeName(typeName);
+
+        Assert.assertEquals(expectedSimpleName, type.getSimpleName());
+        Assert.assertEquals(expectedPackageName, type.getPackageName());
+    }
+}
--- device-registry-client/src/main/java/iot/core/services/device/registry/client/package-info.java
+++ device-registry-client/src/main/java/iot/core/services/device/registry/client/package-info.java
@@ -1,4 +1,5 @@
-@org.iotbricks.annotations.Client(DeviceRegistryService.class)
+@Client(DeviceRegistryService.class)
 package iot.core.services.device.registry.client;
 
+import org.iotbricks.annotations.Client;
 import org.iotbricks.service.device.registry.api.DeviceRegistryService;
