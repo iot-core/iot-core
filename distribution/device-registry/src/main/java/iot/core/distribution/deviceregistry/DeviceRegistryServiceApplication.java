a3519acf58573bbe58f6bd68f584ea3e14d23a5c
Henryk Konsek <hekonsek@gmail.com>
2017-10-05 09:04:23 +0200

    Fixed MongoDB connection setup.

--- distribution/device-registry/src/main/java/iot/core/distribution/deviceregistry/DeviceRegistryServiceApplication.java
+++ distribution/device-registry/src/main/java/iot/core/distribution/deviceregistry/DeviceRegistryServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication(scanBasePackages = {"org.iotbricks", "iot.core"})
-public class DeviceRegistryApplication {
+public class DeviceRegistryServiceApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(DeviceRegistryApplication.class).run(args);
+        new SpringApplicationBuilder(DeviceRegistryServiceApplication.class).run(args);
     }
 
 }
--- service/device-registry/mongo/src/main/java/org/iotbricks/service/device/registry/mongo/spring/MongoDeviceRegistryServiceConfiguration.java
+++ service/device-registry/mongo/src/main/java/org/iotbricks/service/device/registry/mongo/spring/MongoDeviceRegistryServiceConfiguration.java
@@ -1,7 +1,6 @@
 package org.iotbricks.service.device.registry.mongo.spring;
 
 import com.mongodb.MongoClient;
-import de.flapdoodle.embed.mongo.config.IMongodConfig;
 import org.iotbricks.service.device.registry.api.DeviceRegistryService;
 import org.iotbricks.service.device.registry.mongo.MongoDeviceRegistryService;
 import org.iotbricks.service.device.registry.spi.DeviceSchemaValidator;
@@ -16,9 +15,8 @@ import org.springframework.context.annotation.Import;
 public class MongoDeviceRegistryServiceConfiguration {
 
     @ConditionalOnProperty(name = "iotbricks.mongo.enabled", havingValue = "true")
-    @Bean DeviceRegistryService deviceRegistryService(IMongodConfig mongodConfig, DeviceSchemaValidator deviceSchemaValidator) {
-        MongoClient mongoClient = new MongoClient("localhost", mongodConfig.net().getPort());
-        return new MongoDeviceRegistryService(mongoClient, deviceSchemaValidator);
+    @Bean DeviceRegistryService deviceRegistryService(MongoClient mongo, DeviceSchemaValidator deviceSchemaValidator) {
+        return new MongoDeviceRegistryService(mongo, deviceSchemaValidator);
     }
 
 }
